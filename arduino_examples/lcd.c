10c2f8666ba3ee3b27b2fafa11509f531f6e635b
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-01-09 12:58:57 +0200

    Turning on and off LCD at each 5 seconds.

--- arduino_examples/lcd.c
+++ arduino_examples/lcd.c
@@ -8,16 +8,26 @@
 #define LCD_D6 5 // attach pin D5 Arduino to LCD D6
 #define LCD_D5 6 // attach pin D6 Arduino to LCD D5
 #define LCD_D4 7 // attach pin D7 Arduino to LCD D4
+#define LCD_ONOFF 3 // attach pin D3 Arduino to LCD A (15th pin)
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-
+int on = true;
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.print("hello, world!");
+  pinMode(LCD_ONOFF,OUTPUT);
 }
 
 void loop() {
-
+  delay(5000);
+  if(on){
+    digitalWrite(LCD_ONOFF,LOW);
+    on = false;
+  } else{
+    digitalWrite(LCD_ONOFF,HIGH);
+    on = true;
+  }
 }
+
