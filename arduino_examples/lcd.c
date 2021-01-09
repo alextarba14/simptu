95807ff30a8f01a4d194fdea16df94da7a5e7fb4
Tarba Lucian-Alexandru <alextarba14@gmail.com>
2021-01-09 12:50:02 +0200

    Added LCD display example.

---
+++ arduino_examples/lcd.c
@@ -0,0 +1,23 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// Constants
+#define LCD_RS 9 // attach pin D9 Arduino to LCD RS
+#define LCD_EN 8 // attach pin D8 Arduino to LCD E
+#define LCD_D7 4 // attach pin D4 Arduino to LCD D7
+#define LCD_D6 5 // attach pin D5 Arduino to LCD D6
+#define LCD_D5 6 // attach pin D6 Arduino to LCD D5
+#define LCD_D4 7 // attach pin D7 Arduino to LCD D4
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+
+}
